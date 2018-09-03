b366ca7d2b62354288b0e74ca09a7a769d8acbc3
KuroyukiDev <kuroyuki.dev@gmail.com>
2018-09-03 15:18:41 -0700

    Config static serach term triggering <VideoListItem /> to populte listview

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyBcQNUhJIYUNscp74_zc9lef2kAWwfHz_8';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'takedakatsuyori yd professor onigiri pc'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={ this.state.videos } />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
